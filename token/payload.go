b141af872b33d2a9d787aef296d5cf9cbfba0382
Aashish Pant <aaxishpunk31@gmail.com>
2024-06-07 11:23:39 +0545

    PASETO TOKEN

--- token/payload.go
+++ token/payload.go
@@ -33,6 +33,15 @@ func NewPayload(username string, duration time.Duration) (*Payload, error) {
 
 }
 
+func (p *Payload) Valid() error {
+	if time.Now().After(p.Expiry) {
+		return errExpiredToken
+	}
+	return nil
+}
+
+///BELOW METHODS ARE WRITTEN JUST TO IMPLEMENT JWT.CLAIMS INTERFACE
+
 func (p *Payload) GetAudience() (jwt.ClaimStrings, error) {
 	if p == nil {
 		return nil, errors.New("nil pointer receiver")
@@ -51,8 +60,7 @@ func (p *Payload) GetIssuedAt() (*jwt.NumericDate, error) {
 	if p == nil {
 		return jwt.NewNumericDate(time.Time{}), errors.New("nil pointer receiver")
 	}
-	//return jwt.NewNumericDate(p.Issued_at), nil
-	return nil, nil
+	return jwt.NewNumericDate(p.Issued_at), nil
 
 }
 
@@ -60,27 +68,20 @@ func (p *Payload) GetIssuer() (string, error) {
 	if p == nil {
 		return "", errors.New("nil pointer receiver")
 	}
-	//return "your_issuer", nil
-	return "", nil
+	return "your_issuer", nil
+
 }
 func (p *Payload) GetSubject() (string, error) {
 	if p == nil {
 		return "", errors.New("nil pointer receiver")
 	}
-	// return p.ID.String(), nil
-	return "", nil
+	return p.ID.String(), nil
+
 }
 func (p *Payload) GetNotBefore() (*jwt.NumericDate, error) {
 	if p == nil {
 		return jwt.NewNumericDate(time.Time{}), errors.New("nil pointer receiver")
 	}
-	// return jwt.NewNumericDate(time.Time{}), nil
-	return nil, nil
-}
+	return jwt.NewNumericDate(time.Time{}), nil
 
-func (p *Payload) Valid() error {
-	if time.Now().After(p.Expiry) {
-		return errExpiredToken
-	}
-	return nil
 }
