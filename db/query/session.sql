ee70c639c21e436572b8b670165e94fb01beb4e4
Aashish Pant <aaxishpunk31@gmail.com>
2024-10-27 13:53:58 +0545

    sqlc gen

--- db/query/session.sql
+++ db/query/session.sql
@@ -1,3 +1,17 @@
----name: CreateSession :one
-insert into sessions ("name", "refresh_token", "expires_at")
-values ($1, $2, $3, $4)
+-- name: CreateSession :one
+INSERT into sessions (
+    id,
+    username,
+    refresh_token,
+    user_agent,
+    client_ip,
+    is_blocked,
+    expires_at)
+VALUES ($1, $2, $3, $4, $5,$6, $7)
+RETURNING *;
+
+
+-- name: GetSession :one
+
+SELECT * from sessions
+WHERE id=$1 LIMIT 1;
